a7ea5a4ebf41fcf6cfdf31cb18d90727e0ca35ce
sameer shaikh <46072865+sameerjshaikh@users.noreply.github.com>
2019-11-05 08:32:32 -0800

    Update ArrayAssign.java

--- ArrayAssign.java
+++ ArrayAssign.java
@@ -36,12 +36,27 @@ class ArrayAssign
 		}
 		return avg/n;
 	}
+	static int[] reverse(int[] arr)
+	{
+		int n=arr.length;
+
+		int[] varr=new int[n];
+int j=0;
+		for(int i=n-1;i>=0;i--)
+		{
+			varr[j]=arr[i];
+			j++;
+		}
+
+		return varr;
+		
+	}
 
 	public static void main(String[] args)
 	{
 		int size=MyConsole.getNumber("Enter size of array");
 		int[] values=new int[size];
-
+		int[] rev=new int[size];
 			for(int i=0;i<size;i++)
 			{
 				values[i]=MyConsole.getNumber("Enter the Array Element");
@@ -50,6 +65,14 @@ class ArrayAssign
 			System.out.print("The max value is "+maxFun(values));
 			System.out.print("\nThe min value is "+minFun(values));
 			System.out.print("\nThe avg value is "+avgFun(values));
+		//	System.out.print("\nThe avg value is "+reverse(values));
+			rev=reverse(values);
+			System.out.print("\nThe reverse array is \n");
+			for(int i=0;i<size;i++)
+			{
+				System.out.print(rev[i]+" ");
+			}
+
 
 
 	}
